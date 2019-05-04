15b28bc176b3fda8bb66bc185b705e556d65469b
Hediye Tasar <hediyetasar@hotmail.com>
2019-05-04 13:54:29 +0300

    Add files via upload

---
+++ src/learnJava/DisardanDegerAl.java
@@ -0,0 +1,20 @@
+package learnJava;
+
+import java.util.Scanner;
+
+public class DisardanDegerAl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Lütfen 1. Sayýyý giriniz :  ");
+		int sayi1 = scanner.nextInt();
+		System.out.print("Lütfen 2. Sayýyý giriniz :  ");
+		int sayi2 = scanner.nextInt();
+		int carpim = sayi1 * sayi2;
+
+		System.out.println(sayi1 + " x " + sayi2 + " = " + carpim);
+
+	}
+
+}
---
+++ src/learnJava/YasHesabi.java
@@ -0,0 +1,14 @@
+package learnJava;
+
+public class YasHesabi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte cocukYasi=10;	
+		byte babaYasi=35;
+
+		byte yasFarki=(byte)(babaYasi-cocukYasi);
+		System.out.println(yasFarki);
+	}
+
+}
