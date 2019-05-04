9d6800b371753afde3c520cd533533126b035735
Hediye Tasar <hediyetasar@hotmail.com>
2019-05-04 12:03:14 +0300

    Add files via upload

---
+++ src/org/ismek/Carpim.java
@@ -0,0 +1,14 @@
+package org.ismek;
+
+public class Carpim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// syso Ctrl+Space
+		int sayi=3;
+		for(int i=1; i<=5; i++) {
+			System.out.println(sayi+" x "+i+" = "+(sayi*i));
+		}
+	}
+
+}
